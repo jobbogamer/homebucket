a9a6ff129168b9c32cf45e6d62408d9cb9c2ed13
Josh Asch <josh.asch@gmail.com>
2014-11-30 14:00:47 +0000

    Change to python

---
+++ homebucket.py
@@ -0,0 +1,35 @@
+
+import newrelic.agent
+import os
+from flask import Flask, url_for, render_template, request, jsonify
+
+##### Config #####
+
+newrelic.agent.initialize('newrelic.ini')
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "d47d2b74ff64e5a6ae5aedd4edebeaf1"
+
+try:
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+except KeyError as error:
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://localhost:5432"
+
+##### Routes #####
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+##### API Routes #####
+
+
+
+##### Template Filters #####
+
+
+
+##### Main #####
+
+if (__name__ == "__main__"):
+    app.run(debug=True)
